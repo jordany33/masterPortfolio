0bc8fbb52a3e60679dbcf3e0e85a4a76fec34706
jordany33 <jrdeyee@gmail.com>
2023-07-21 18:39:21 -0700

    random responsive dots in the background.

--- masterPortfolio/script.js
+++ masterPortfolio/script.js
@@ -1,28 +1,41 @@
-// Tilt Effect
-const tiltElement = document.querySelector(".tilt-element");
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-document.addEventListener("mousemove", (event) => {
-    const xPosition = (window.innerWidth / 2 - event.pageX) / 25;
-    const yPosition = (window.innerHeight / 2 - event.pageY) / 25;
+const numBalls = 50;
+const balls = [];
 
-    tiltElement.style.transform = `rotateY(${xPosition}deg) rotateX(${yPosition}deg)`;
-});
-
-document.addEventListener("mouseleave", () => {
-    tiltElement.style.transform = "rotateY(0deg) rotateX(0deg)";
-});
-
-// Typewriter Effect
-const text = "Welcome to My Portfolio! I'm a Software Engineer.";
-
-let index = 0;
-function typewriterEffect() {
-    const typewriterElement = document.getElementById("typewriter-effect");
-    if (index < text.length) {
-        typewriterElement.textContent += text.charAt(index);
-        index++;
-        setTimeout(typewriterEffect, 50); // Adjust the speed of typewriter effect here
-    }
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
 }
 
-typewriterEffect();
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
