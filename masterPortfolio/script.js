070e3d25b1299a2cb12ee0eb2e838f62b4fd8aea
jordany33 <jrdeyee@gmail.com>
2023-07-21 19:57:31 -0700

    added typewriter effect.

--- masterPortfolio/script.js
+++ masterPortfolio/script.js
@@ -39,3 +39,54 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+// Typewriter effect
+let i = 0;
+let j = 0;
+const words = ["create", "imagine"];
+let currentWord = "";
+let isDeleting = false;
+let isWaiting = false;
+let speed = 60;
+
+function typeWriter() {
+  if (isWaiting) return;
+
+  if (!isDeleting && i <= words[j].length) {
+    currentWord = words[j].substring(0, i);
+    i++;
+  }
+
+  if (isDeleting && i >= 0) {
+    currentWord = words[j].substring(0, i);
+    i--;
+  }
+
+  document.querySelector('.typing').textContent = currentWord;
+
+  // when typing and deleting, hide the blinker
+  if (!isWaiting) {
+    document.querySelector('.typing').style.animation = 'none';
+  } else {
+    document.querySelector('.typing').style.animation = 'blink 1s infinite';
+  }
+
+  if (i == words[j].length + 1) {
+    isDeleting = true;
+    setTimeout(() => {
+      isWaiting = false;
+      typeWriter();
+    }, 1500); // Change the pause duration here
+    isWaiting = true;
+  }
+
+  if (i == 0 && isDeleting) {
+    isDeleting = false;
+    j = j == words.length - 1 ? 0 : j + 1;
+  }
+
+  setTimeout(typeWriter, speed);
+}
+
+window.onload = typeWriter;
+
