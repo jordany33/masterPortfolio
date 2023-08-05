98e73472b7e43765daaf6a7da27240daca3dd60d
hmodule <50082611+hmodule@users.noreply.github.com>
2023-08-05 08:41:17 -0400

    new stuff (feat. kyle)
    - added morgan for route logging
    - added basic data collection on page visits, see '/logger'
    
    NOTE: since a new package was added, you must run 'npm i' or 'npm i morgan' to get the required package(s)

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const express = require( 'express' );
+const session = require( 'express-session' );
 const router = express.Router();
+const morgan = require( 'morgan' );
 
 const app = express();
 const port = process.env.PORT || 1200;
 
+app.use( express.json() )
+app.use( express.urlencoded( { extended: true } ) );
+app.use( morgan( 'dev' ) ); // request logger
+
+/* NOTE: 
+The order of the 2 functions below is critical for the app to work
+properly. The session middleware wont be called for any 
+requests that get handled by the router if the router is 
+initalized before the session data is set.
+( if !session, the router will continue as normal )
+*/
+
+app.use( session( { secret: 'xQN7Ep8NjsZjzy', resave: true, saveUninitialized: true, cookie: { maxAge: 100000, secure: false } } ) );
 app.use( '/', router ); /* this is what allows us to specify routes */
 
+
 // setup all static files (css, html, scripts)
-app.use( '/views', express.static('../views'))
-app.use( express.static( '../imgs' ) );
-app.use( '/scripts', express.static('../scripts') )
+app.use( '/', express.static( '../' ) );
 
 
 // get root dir (https://jordany.dev/) & send to index.html
-router.get( '/', ( req, res ) => 
+router.get( '/', ( req, res, next ) => 
 {
     res.sendFile( 'index.html', { root: '../views' } );
 });
@@ -22,6 +36,29 @@ router.get( '/', ( req, res ) =>
 router.get( '/projects', ( req, res ) => 
 {   
     res.sendFile( 'projects.html', { root: '../views' });
+});
+
+router.get( '/logger', ( req, res, next ) => 
+{
+    if ( req.session.views )
+    {
+        let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+        req.session.views ++
+        res.setHeader( 'Content-Type', 'text/html' );
+        res.write( '<p>session_id: ' + req.sessionID + '</p>' )
+        res.write( '<p>path: ' + req.path + '</p>' )
+        res.write( '<p>views: ' + req.session.views + '</p>' );
+        res.write( '<p>expires in: ' + ( req.session.cookie.maxAge / 1000 ) + 's</p>' );
+        res.write( '<p>IP: ' + ip + '</p>' )
+        res.end();
+    }
+    else
+    {
+        req.session.views = 1;
+        res.send( 'welcome, refresh to start logging!' );
+    }
+    next()
 });
 
 app.listen( port );
